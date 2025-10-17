77c0cea3f40dfa24ff61fb5efa36c92bd6e1b441
Manganyi Harmony <221045724@edu.vut.ac.za>
2025-10-17 14:38:53 +0200

    train model

--- CarePredict-AI/Main.py
+++ CarePredict-AI/Main.py
@@ -45,6 +45,12 @@ y_risk = df['Risk_Level']
 
 
 
+# ============================================================================
+# TRAIN MODEL
+# ============================================================================
+X_train, X_test, y_train, y_test = train_test_split(X, y_risk, test_size=0.2, random_state=42)
+model_risk = XGBClassifier(eval_metric='mlogloss')
+model_risk.fit(X_train, y_train)
 
 
 
